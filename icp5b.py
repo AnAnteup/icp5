41f308db3c4773f7866665651317a4edd18bb576
haozhang <332915343@qq.com>
2019-02-25 23:36:40 -0600

    update

--- icp5b.py
+++ icp5b.py
@@ -6,9 +6,9 @@ data= pd.read_csv('College.csv')
 
 data=data.drop(columns=['Name','Private'],axis=1)
 
-for i in range(2,7):
+for i in range(2,18):
     kmean = KMeans(n_clusters=i, max_iter=300)
     x= kmean.fit_predict(data)
 
     sil=silhouette_score(data,x)
-    print("for cluster ",i, " the silhouette score is",sil)
+    print( " Silhouette analysis for KMeans clustering",i," on sample data is",sil)
